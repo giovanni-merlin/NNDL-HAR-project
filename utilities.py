d0f306d034c93c7d148281b804914a709e9a7e19
Giovanni Merlin <115490914+giovanni-merlin@users.noreply.github.com>
2024-09-21 23:44:26 +0200

    moved macros to create train test and val sets

--- utilities.py
+++ utilities.py
@@ -40,8 +40,7 @@ class CSIDataset(Dataset):
         return (csi_data, label_tensor)
     
 
-def create_dataset_single(csi_matrix_files, labels_stride, stream_ant, input_shape, batch_size, shuffle, #NB cache file is not used!
-                          cache_file=None, prefetch=True, repeat=False):
+def create_dataset_single(csi_matrix_files, labels_stride, stream_ant, input_shape, batch_size, shuffle, prefetch=True, repeat=False):
     dataset = CSIDataset(csi_matrix_files, labels_stride, stream_ant, input_shape)
 
     if repeat: # this is not even used!
@@ -73,3 +72,171 @@ def load_data_single(csi_file_t, stream_a):
 
     matrix_csi_single = torch.tensor(matrix_csi_single, dtype=torch.float32) #vedi che fa
     return matrix_csi_single
+
+
+def create_training_set(dir_init, subdirs_init, feature_length_init = 100, sample_length_init = 340, channels_init = 1, batch_size_init = 32, num_tot_init = 4, activities_init = 'E,L,W,R,J'):
+    """
+    Create the training set from the raw data.
+    We can specify the input dimension, the batch size, and other things.
+    Returns a Dataloader object with the input and label batches to iterate on.
+    Inputs: dir_init: path of the data directory (str)
+            subdirs_init: path of the data subdirectories (str)
+            feature_length_init: length along the feature dimension (height)
+            sample_length_init: length along the time dimension (width)
+            channels_init: number of channels
+            batch_size_init: number of samples in a batch
+            num_tot_init: number of antenna * number of spatial streams
+            activities_init: activities to be considered
+    Return: Dataloader obj
+    """
+
+    activities = activities_init 
+    subdirs_training = subdirs_init
+    labels_train = []
+    all_files_train = []
+    sample_length = sample_length_init
+    feature_length = feature_length_init
+    channels = channels_init
+    num_antennas = num_tot_init
+    input_network = (sample_length, feature_length, channels) # maybe better call input shape this?
+    batch_size = batch_size_init
+    output_shape = len(activities.split(','))
+    labels_considered = np.arange(output_shape)
+
+    suffix = '.txt'
+
+    for sdir in subdirs_training.split(','):
+        
+        #dir_train = dir_init + sdir + '/train_antennas_' + str(activities) + '/'
+        name_labels = dir_init + sdir + '/labels_train_antennas_' + str(activities) + suffix
+        with open(name_labels, "rb") as fp:  # Unpickling
+            labels_train.extend(pickle.load(fp))
+        name_f = dir_init + sdir + '/files_train_antennas_' + str(activities) + suffix
+        with open(name_f, "rb") as fp:  # Unpickling
+            all_files_train.extend(pickle.load(fp))
+
+    # create the train dataset
+    file_train_selected = [all_files_train[idx] for idx in range(len(labels_train)) if labels_train[idx] in
+                            labels_considered]
+    labels_train_selected = [labels_train[idx] for idx in range(len(labels_train)) if labels_train[idx] in
+                                labels_considered]
+
+    file_train_selected_expanded, labels_train_selected_expanded, stream_ant_train = \
+        expand_antennas(file_train_selected, labels_train_selected, num_antennas)
+
+    dataset_csi_train = create_dataset_single(file_train_selected_expanded, labels_train_selected_expanded,
+                                                stream_ant_train, input_network, batch_size,
+                                                shuffle=True)
+    
+    return dataset_csi_train
+
+
+def create_validation_set(dir_init, subdirs_init, feature_length_init = 100, sample_length_init = 340, channels_init = 1, batch_size_init = 32, num_tot_init = 4, activities_init = 'E,L,W,R,J'):
+    """
+    Create the validation set from the raw data.
+    We can specify the input dimension, the batch size, and other things.
+    Returns a Dataloader object with the input and label batches to iterate on.
+    Inputs: dir_init: path of the data directory (str)
+            subdirs_init: path of the data subdirectories (str)
+            feature_length_init: length along the feature dimension (height)
+            sample_length_init: length along the time dimension (width)
+            channels_init: number of channels
+            batch_size_init: number of samples in a batch
+            num_tot_init: number of antenna * number of spatial streams
+            activities_init: activities to be considered
+    Return: Dataloader obj
+    """
+
+    activities = activities_init 
+    subdirs_training = subdirs_init
+    labels_val = []
+    all_files_val = []
+    sample_length = sample_length_init
+    feature_length = feature_length_init
+    channels = channels_init
+    num_antennas = num_tot_init
+    input_network = (sample_length, feature_length, channels) # maybe better call input shape this?
+    batch_size = batch_size_init
+    output_shape = len(activities.split(','))
+    labels_considered = np.arange(output_shape)
+
+    suffix = '.txt'
+
+    for sdir in subdirs_training.split(','):
+
+        #dir_val = dir_init + sdir + '/val_antennas_' + str(activities) + '/'
+        name_labels = dir_init + sdir + '/labels_val_antennas_' + str(activities) + suffix
+        with open(name_labels, "rb") as fp:  # Unpickling
+            labels_val.extend(pickle.load(fp))
+        name_f = dir_init + sdir + '/files_val_antennas_' + str(activities) + suffix
+        with open(name_f, "rb") as fp:  # Unpickling
+            all_files_val.extend(pickle.load(fp))
+
+    # create the validation dataset
+    file_val_selected = [all_files_val[idx] for idx in range(len(labels_val)) if labels_val[idx] in
+                            labels_considered]
+    labels_val_selected = [labels_val[idx] for idx in range(len(labels_val)) if labels_val[idx] in
+                            labels_considered]
+
+    file_val_selected_expanded, labels_val_selected_expanded, stream_ant_val = \
+        expand_antennas(file_val_selected, labels_val_selected, num_antennas)
+
+    dataset_csi_val = create_dataset_single(file_val_selected_expanded, labels_val_selected_expanded,
+                                            stream_ant_val, input_network, batch_size,
+                                            shuffle=False)
+    return dataset_csi_val
+
+
+def create_test_set(dir_init, subdirs_init, feature_length_init = 100, sample_length_init = 340, channels_init = 1, batch_size_init = 32, num_tot_init = 4, activities_init = 'E,L,W,R,J'):
+    """
+    Create the test set from the raw data.
+    We can specify the input dimension, the batch size, and other things.
+    Returns a Dataloader object with the input and label batches to iterate on.
+    Inputs: dir_init: path of the data directory (str)
+            subdirs_init: path of the data subdirectories (str)
+            feature_length_init: length along the feature dimension (height)
+            sample_length_init: length along the time dimension (width)
+            channels_init: number of channels
+            batch_size_init: number of samples in a batch
+            num_tot_init: number of antenna * number of spatial streams
+            activities_init: activities to be considered
+    Return: Dataloader obj
+    """
+
+    activities = activities_init 
+    subdirs_training = subdirs_init
+    labels_test = []
+    all_files_test = []
+    sample_length = sample_length_init
+    feature_length = feature_length_init
+    channels = channels_init
+    num_antennas = num_tot_init
+    input_network = (sample_length, feature_length, channels) # maybe better call input shape this?
+    batch_size = batch_size_init
+    output_shape = len(activities.split(','))
+    labels_considered = np.arange(output_shape)
+
+    suffix = '.txt'
+
+    for sdir in subdirs_training.split(','):
+        #dir_test = dir_init + sdir + '/test_antennas_' + str(activities) + '/'
+        name_labels = dir_init + sdir + '/labels_test_antennas_' + str(activities) + suffix
+        with open(name_labels, "rb") as fp:  # Unpickling
+            labels_test.extend(pickle.load(fp))
+        name_f = dir_init + sdir + '/files_test_antennas_' + str(activities) + suffix
+        with open(name_f, "rb") as fp:  # Unpickling
+            all_files_test.extend(pickle.load(fp))
+
+    # create the test dataset
+    file_test_selected = [all_files_test[idx] for idx in range(len(labels_test)) if labels_test[idx] in
+                            labels_considered]
+    labels_test_selected = [labels_test[idx] for idx in range(len(labels_test)) if labels_test[idx] in
+                            labels_considered]
+
+    file_test_selected_expanded, labels_test_selected_expanded, stream_ant_test = \
+        expand_antennas(file_test_selected, labels_test_selected, num_antennas)
+
+    dataset_csi_test = create_dataset_single(file_test_selected_expanded, labels_test_selected_expanded,
+                                                stream_ant_test, input_network, batch_size,
+                                                shuffle=False)
+    return dataset_csi_test
